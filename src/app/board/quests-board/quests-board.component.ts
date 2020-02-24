b55f958bb3171d52e6f9ec741e167c06fbd0f948
KoshcheevPA <42152174+KoshcheevPA@users.noreply.github.com>
2020-02-25 02:31:42 +0300

    task completed

--- src/app/board/quests-board/quests-board.component.ts
+++ src/app/board/quests-board/quests-board.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { QuestService } from '../../shared/quest.service';
 import { IQuest } from '../../shared/quest';
+import { Location } from '@angular/common'
 
 @Component({
   selector: 'app-quests-board',
@@ -9,16 +10,23 @@ import { IQuest } from '../../shared/quest';
 })
 export class QuestsBoardComponent implements OnInit {
   quests: IQuest[] = [];
+  questsType:Boolean = false;
 
-  constructor(private questService: QuestService) { }
+  constructor(private questService: QuestService, private location: Location) {
+  }
 
   ngOnInit() {
+    if (this.location.path() === '/my-quests') {
+      this.questsType = true;
+    } else if (this.location.path() === '/board'){
+      this.questsType = false;
+    }
+
     this.questService.getQuests()
       .subscribe(quests => {
         if (quests && quests.length) {
-          this.quests = quests;
+          this.quests = quests.filter(el => el.isAccepted === this.questsType);
         }
       });
   }
-
 }
